a3fec00b24d5013dbee41f715656a485f790ef54
Sean Nealon <signalflowsean@gmail.com>
2018-12-11 12:16:22 -0500

    factorial/

--- factorial.js
+++ factorial.js
@@ -1,7 +1,19 @@
-function fact(num){
+function factR(num){
   if(num === 1){
     return 1;
   }
-  return  num * fact(num-1);
+  return  num * factR(num-1);
 }
-console.log(fact(8));
+
+function factI(num){ 
+  let fact = 1; 
+  for(let i=2; i <= num; i++){ 
+    fact *= i; 
+  }
+  return fact; 
+}
+
+//O(n)
+console.log(factR(8));
+//O(n)
+console.log(factI(8)); 
